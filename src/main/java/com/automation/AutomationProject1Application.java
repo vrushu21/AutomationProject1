e6a5d2f813d51c58168f8049589df73d58190dad
vrushu21 <vrushu.medankar@gmail.com>
2020-04-28 18:30:30 +0100

    Initial project structure

---
+++ src/main/java/com/automation/AutomationProject1Application.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/automation/AutomationProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
